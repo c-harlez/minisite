0df43fd855ab17ee13535fa68c6d06f471a30de3
root <root@46-typo3-dev.ip-5-39-66.eu>
2015-02-23 10:01:48 -0500

    - Fix Backend layout - fix minors bugs

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,5 @@
 if (!defined('TYPO3_MODE')) {
 die('Access denied.');
 }
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/web_layout.txt">');
