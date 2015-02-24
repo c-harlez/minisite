34887f436eabd3bcd6fb1d4dc9c1a532c920dc60
root <root@46-typo3-dev.ip-5-39-66.eu>
2015-02-24 05:10:43 -0500

    * Détachement de la colonne de gauche dans Partials/Page/Structure/Gauche pour heritage du contenu

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,3 +4,10 @@ die('Access denied.');
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/web_layout.txt">');
+
+$rootlinefields = &$GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"];
+if($rootlinefields != '')
+{
+    $rootlinefields .= ' , ';
+}
+$rootlinefields .= 'pageGlyphicon';
